8a75507d4c8d3c14a28c2fa7477160a895f54d4a
Steven E. Pav <shabbychef@gmail.com>
2013-11-20 13:48:57 -0800

    staring HW for week7

---
+++ week7/homework1.R
@@ -0,0 +1,13 @@
+# Week 7 Homework 1: 
+#
+# * Wed Nov 20 2013 01:48:39 PM Steven E. Pav <steven@cerebellumcapital.com>
+#
+#
+
+
+fdisp <- function(x) {
+	cat("# ",x,"\n")
+}
+
+#for vim modeline: (do not edit)
+# vim:fdm=marker:fmr=FOLDUP,UNFOLD:cms=#%s:syn=r:ft=r
